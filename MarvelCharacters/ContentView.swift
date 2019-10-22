c553216308decc0963c3c17aa07236655b1b07bb
Gabriel Henrique Paul <gabriel.paul@southsystem.com.br>
2019-10-22 17:27:54 -0300

    Initial Commit

---
+++ MarvelCharacters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelCharacters
+//
+//  Created by Gabriel Paul on 22/10/19.
+//  Copyright © 2019 GabrielPaul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
